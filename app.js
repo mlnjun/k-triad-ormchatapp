2a47e8630f61d2f7d67fd69217bca9796d38d3e6
minjun <rhalswns098@gmail.com>
2023-12-13 16:07:03 +0900

    new: channel.js, channelAPI.js 라우터 생성

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var channelRouter = require('./routes/channel');
+var channelAPIRouter = require('./routes/channelAPI');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/chat', channelRouter);
+app.use('/api/channel', channelAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/channelAPI.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var router = express.Router();
+
+
+/*
+- 채널/채팅 정보 관리 RESTful API 라우팅 기능 제공
+http://localhost:3000/api/channel
+*/
+
+// 채널 데이터
+let channelData = [
+  {
+    chName : "A",
+    chDescription : "A 채널",
+    chid : 1
+  },
+  {
+    chName : "B",
+    chDescription : "B 채널",
+    chid : 2
+  },
+  {
+    chName : "C",
+    chDescription : "C 채널",
+    chid : 3
+  }
+]
+
+
+// 회원 정보 관리 RESTful API 라우팅 기능 제공
+router.get('/all',async(req, res)=>{
+  res.json(channelData);
+});
+
+
+router.post('/create',async(req,res)=>{
+  var chName = req.body.chName;
+  var chDescription = req.body.chDescription;
+  var chid = req.body.chid;
+
+  var newChdata = {
+    chName,
+    chDescription,
+    chid
+  }
+
+  channelData.push(newChdata);
+
+  // DB에 데이터 저장
+
+  // 저장 후 
+  res.json(channelData);
+
+})
+
+
+
+// http://localhost:3000/api/channel/modify
+router.post('/modify',async(req,res)=>{
+  let {chName, chDescription, chid} = req.body;
+
+
+  let modifyChdata = {
+    chName,
+    chDescription,
+    chid
+  }
+
+  channelData.push(modifyChdata);
+
+
+  res.json(channelData);
+
+})
+
+
+// 채널 삭제
+// http://localhost:3000/api/channel/delete
+router.post('/delete',async(req,res)=>{
+  let chid = req.body.chid;
+
+  let index
+
+
+  for(let i = 0; i<channelData.length; i++){
+    if(channelData[i].chid == chid){
+      channelData.splice(i, 1);
+      // console.log(channelData[i]);j
+      res.json(channelData);
+      index = i;
+      break;
+    }
+  }
+
+
+  if(channelData[index] == undefined){
+    res.send("해당 계정은 존재하지 않습니다.");
+  }
+});
+
+
+// 단일 채널 찾기
+// http://localhost:3000/api/channel/cid
+router.get('/cid/:chName',async(req, res)=>{
+  let chName = req.params.chName;
+
+
+  for(let i = 0; i<channelData.length; i++){
+    if(channelData[i].chName == chName){
+      res.json(channelData[i]);
+      break;
+    }
+  }
+  
+
+})
+
+
+
+module.exports = router;
