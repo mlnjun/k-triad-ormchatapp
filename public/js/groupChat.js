cabd459ec8ffae21e9ce2f54914af2c53db68cdc
ursrBaek <ursrbaek@gmail.com>
2024-01-26 01:58:39 +0900

    feat: 그룹채팅 생성 모달의 각종 이벤트핸들러 등록

--- public/js/groupChat.js
+++ public/js/groupChat.js
@@ -1,11 +1,31 @@
-let invitedMembers = [];
+let invitedMembers = [{ member_id: '1' }, { member_id: '2' }, { member_id: '3' }, { member_id: 4 }];
 const loginUserToken = localStorage.getItem('userAuthToken');
 
-// ========================================================
-// 멤버 x 버튼 누르면 제외시키기
-// ========================================================
+$('#GroupChatFormModalCloseBtn').click(clearGroupChatForm);
+$('#createGroup').click(function (e) {
+  var modal = document.getElementById('groupChatCreateModal');
+  if (e.target !== modal && !modal.contains(e.target)) {
+    // 클릭된 요소가 모달과 모달의 하위 요소들이 아니면 모달을 닫음
+    console.log('설마 이 콜백은 아니겠지');
+    console.log(e.target);
+    console.log(modal.contains(e.target));
+    clearGroupChatForm();
+  }
+});
 
-$('#inviteMemberBtn').click(function () {
+$('#invitedMemberList').click(function (e) {
+  e.stopPropagation();
+
+  if (e.target.dataset.memberId) {
+    invitedMembers = invitedMembers.filter((member) => {
+      return member.member_id != e.target.dataset.memberId;
+    });
+  }
+
+  console.log(invitedMembers);
+});
+
+$('#inviteMemberAddBtn').click(function () {
   const invitedMemberEmail = $('#invitedMemberEmail').val();
 
   if (invitedMemberEmail) {
@@ -34,14 +54,13 @@ $('#inviteMemberBtn').click(function () {
           const addedMember = result.data;
 
           // invitedMembers에 추가
-          invitedMembers.push(result.data);
-
-          // ============================
+          invitedMembers.push(addedMember);
           // invitedMembers 리스트 화면에 반영
-          // ============================
+          appendMemberToList(addedMember);
         } else if (result.code == 400) {
           alert(result.msg);
           $('#invitedMemberEmail').val('');
+          $('#invitedMemberEmail').focus();
         }
       },
       error: function (err) {
@@ -51,39 +70,43 @@ $('#inviteMemberBtn').click(function () {
   }
 });
 
-$('#createGroupChatForm').submit(function () {
+$('#createGroupChatForm').submit(async function () {
   const channel_name = $('#channel_name').val();
-  // ============================
-  // 채널명 중복 확인 (api 만들어지면 하기)
-  // ============================
-  // $.ajax();
 
+  // ===================================
+  // 이미지가 있다면 이미지 먼저 보내서 저장하고 그 경로 받아서 처리해야할것임...
+  // if ('이미지가 있다면') {
+  // const 이미지 경로 = await 이미지 디비 저장 요청 api 결과인 이미지 패스
+  // }
+  // ===================================
   if (invitedMembers.length) {
-    $.ajax({
+    await $.ajax({
       type: 'POST',
       url: '/api/channel/addchatgroup',
       headers: {
         Authorization: `Bearer ${loginUserToken}`,
       },
       data: {
-        channel_name,
-        invitedMembers,
-        channel_img_path: 'img/close.svg',
+        channelName: channel_name,
+        members: invitedMembers,
+        profile: 'img/close.svg',
       },
       dataType: 'json',
       success: function (result) {
         if (result.code == 200) {
+          alert('그룹채팅 생성이 완료되었습니다.');
+          $('#GroupChatFormModalCloseBtn').click();
           // ============================
-          // 채팅생성 창 닫기
-          // 그룹 채팅 목록 조회 다시 해서 뿌리기
+          // 내가 속한 그룹 채팅 목록 조회 다시 해서 뿌리기 ~
           // ============================
+        } else if (result.code == 400) {
+          // 채널명 중복시 경고창
+          alert(result.msg);
         }
       },
       error: function (err) {
         console.log('api 호출 에러: ', err);
-        // ============================
-        // 화면에 에러난거 표시하기
-        // ============================
+        alert('그룹채팅 생성 중 문제가 발생했습니다.');
       },
     });
   } else {
@@ -91,3 +114,57 @@ $('#createGroupChatForm').submit(function () {
   }
   return false;
 });
+
+$('#uploadGroupChatImg').change(previewImage);
+
+// =========== 함수들 정의 ===================
+
+function makeStackedUserHTML(member) {
+  return `<div class="stacked-user">
+            <img src="${member.profile_img_path}" alt="User" />
+            <span class="delete-user" data-member-id="${member.member_id}">
+              <img src="img/close.svg" alt="Remove User" data-member-id="${member.member_id}" />
+            </span>
+          </div>`;
+}
+
+function appendMemberToList(member) {
+  const memberElem = makeStackedUserHTML(member);
+
+  $('#invitedMemberList').append(memberElem);
+}
+
+function renderInviteMembers(members) {
+  $('#invitedMemberList').empty();
+
+  members.forEach((member) => {
+    appendMemberToList(member);
+  });
+}
+
+function clearGroupChatForm() {
+  console.log('clearGroupChatForm');
+  $('#uploadGroupChatImg').val('');
+  $('#invitedMemberEmail').val('');
+  $('#channel_name').val('');
+  invitedMembers = [];
+  $('#invitedMemberList').empty();
+}
+
+function previewImage() {
+  var input = document.getElementById('uploadGroupChatImg');
+  var preview = document.getElementById('previewImage');
+  var file = input.files[0];
+
+  var reader = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  };
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = 'img/group2.svg'; // 기본 이미지 설정
+  }
+}
