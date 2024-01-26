7dc63ad15138c176ed3dd68cc6884e81e10651e4
“leenabi99git config --global user.name “leenabi99 <lyh0922@empas.com>
2024-01-26 15:23:01 +0900

    그룹 채널 목록 조회

--- public/js/groupChat.js
+++ public/js/groupChat.js
@@ -129,7 +129,50 @@ $('#createGroupChatForm').submit(async function () {
           alert('그룹채팅 생성이 완료되었습니다.');
           $('#GroupChatFormModalCloseBtn').click();
           // ============================
-          // 내가 속한 그룹 채팅 목록 조회 다시 해서 뿌리기 ~
+        // 그룹 채널 목록 조회
+$("#group-icon").click(function () {
+  var loginUserToken = localStorage.getItem("userauthtoken");
+
+  // AJAX로 사용자가 속한 그룹 채널 목록 조회 데이터 바인딩 처리
+  $.ajax({
+      type: "POST",
+      url: "/api/channel/channelsByMember",
+      headers: {
+          Authorization: `Bearer ${loginUserToken}`,
+      },
+      dataType: "json",
+      success: function (result) {
+          console.log("사용자가 속한 그룹 채널 목록 조회 결과: ", result);
+
+          if (result.code == 200) {
+              // 최초 UL 태그내 LI 태그 모두 삭제처리
+              $(".groups-list").html("");
+
+              $.each(result.data, function (index, channel) {
+                  var groupTag = `<li onClick="fnGroupChatEntry(${channel.channel_Id},'${channel.channel_Name}',2);">
+                          <a href="#">
+                              <div class="groups-list-body">
+                                  <div class="groups-msg">
+                                      <h6 class="text-truncate">${channel.channel_Name}</h6>
+                                      <p class="text-truncate">그룹 채널 멤버 수: ${channel.memberCount}</p>
+                                  </div>
+                              </div>
+                          </a>
+                      </li>`;
+
+                  $(".groups-list").append(groupTag);
+              });
+          } else {
+              if (result.code == 400) {
+                  alert(result.code);
+              }
+          }
+      },
+      error: function (err) {
+          console.log("백엔드 API 호출 에러발생:", err);
+      },
+  });
+});
           // ============================
         } else if (result.code == 400) {
           // 채널명 중복시 경고창
